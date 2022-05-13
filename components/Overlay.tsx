76dbc68ccd37e00a544ff60ff0eb5d84c60fdf82
inpatr <i.traub@posteo.de>
2022-05-13 12:09:40 +0200

    basic layout

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,3 +1,15 @@
-const Overlay = () => {};
+interface Props {
+  title: string;
+  location: string;
+}
+
+const Overlay = ({ title, location }: Props) => {
+  return (
+    <div className="flex-auto">
+      <p>{title}</p>
+      <p>{location}</p>
+    </div>
+  );
+};
 
 export default Overlay;
