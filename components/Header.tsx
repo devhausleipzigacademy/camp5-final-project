3cbcbdd483b985e386f63511218e96ac1098053e
inpatr <i.traub@posteo.de>
2022-05-13 15:06:30 +0200

    added dynamic title logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,15 @@
+import { useRouter } from "next/router";
+
 interface Props {
   pagename: string;
 }
 
 const Header = () => {
-  const pagename = "Dashboard";
+  const router = useRouter();
+  let pagename = router.asPath;
+  if (pagename === "/") {
+    pagename = "Dashboard";
+  }
   return (
     <div className="fixed top-0 bg-primary text-primary-text w-full h-16 text-center p-4">
       <h3 className="text-base font-poppins">{pagename}</h3>
@@ -12,3 +18,7 @@ const Header = () => {
 };
 
 export default Header;
+
+// header title has to change dynamically
+// if route = "/" then Dashboard
+// if route = "/{pageName}/.." then {pageName}
