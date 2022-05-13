a1c37367b6a1843c6dd69f780291593369442a10
inpatr <i.traub@posteo.de>
2022-05-13 16:57:42 +0200

    created component with example divs

---
+++ components/Map.tsx
@@ -0,0 +1,9 @@
+const MapComponent = () => {
+  return (
+    <div className="flex flex-end h-[100vh] w-full bg-secondary p-4">
+      <div className="bg-primary w-full h-full"></div>
+    </div>
+  );
+};
+
+export default MapComponent;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import MapComponent from "../components/Map";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MapComponent />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
