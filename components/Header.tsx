5de1b052361ec5e04079e8762208b3718a9e8dd6
inpatr <i.traub@posteo.de>
2022-05-13 14:48:30 +0200

    font works

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ interface Props {
 const Header = () => {
   const pagename = "Dashboard";
   return (
-    <div className="fixed top-0 bg-primary text-primary-text">
-      <h3>{pagename}</h3>
+    <div className="fixed top-0 bg-primary text-primary-text w-full h-16 text-center p-4">
+      <h3 className="text-base font-poppins">{pagename}</h3>
     </div>
   );
 };
