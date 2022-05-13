8c2a6c624521459b61e9399826dcb460ad1cdb61
Dean Wallflower <dean@deanwallflower.com>
2022-05-13 16:37:45 +0200

    working on image carousel

--- components/Image/ItemImage.tsx
+++ components/Image/ItemImage.tsx
@@ -1,4 +1,11 @@
 import { useState } from "react";
+import Image from "next/image";
+import useEmblaCarousel, {
+  EmblaCarouselType,
+  EmblaOptionsType,
+  EmblaPluginType,
+  EmblaEventType,
+} from "embla-carousel";
 
 interface ImgProps {
   src: string[];
@@ -6,11 +13,36 @@ interface ImgProps {
 }
 
 const ItemImage = ({ src, fav }: ImgProps) => {
-  const [images, setImages] = useState<String[]>([
-    "https://images.pexels.com/photos/11953263/pexels-photo-11953263.jpeg?cs=srgb&dl=pexels-ruslan-sikunov-11953263.jpg&fm=jpg",
-    "https://www.pexels.com/photo/bouquet-of-flowers-placed-in-vase-6069736/",
-  ]);
-  return <Image src={src}></Image>;
+  const [images, setImages] = useState<string[]>(src);
+  const [counter, setCounter] = useState<number>(0);
+  const slideHandler = (dir: String) => {
+    console.log(dir, counter);
+      switch (dir) {
+        case "Left":
+          console.log(dir, counter);
+          setCounter((prev) => {counter === 0 : setCounter(0) ? prev - 1});
+          break;
+        case "Right":
+          console.log(dir, counter);
+          console.log(src);
+          setCounter((prev) => prev + 1);
+          break;
+      }
+    }
+  };
+  return (
+    <>
+      <button onClick={() => slideHandler("Left")}>{"<"}</button>
+      <Image
+        alt=""
+        src={images[counter]}
+        layout={"responsive"}
+        width={75}
+        height={75}
+      ></Image>
+      <button onClick={() => slideHandler("Right")}>{">"}</button>
+    </>
+  );
 };
 
 export default ItemImage;
--- pages/item.tsx
+++ pages/item.tsx
@@ -1,7 +1,15 @@
+import ItemImage from "../components/Image/ItemImage";
+
+const imgArray = [
+  "https://upload.wikimedia.org/wikipedia/commons/f/ff/Wikipedia_logo_593.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/7/78/Small_scream.png",
+];
+
 export default function Item() {
   return (
     <>
       <div className="w-full py-4 bg-primary text-center">Hello World</div>
+      <ItemImage src={imgArray} fav={false} />
     </>
   );
 }
