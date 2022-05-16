7bd700de6626e98cdf5ab306aec149142296fd83
inpatr <i.traub@posteo.de>
2022-05-16 12:29:39 +0200

    req works

--- pages/api/items/index.ts
+++ pages/api/items/index.ts
@@ -1,34 +1,17 @@
+import { PrismaClient } from ".prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const prisma = new PrismaClient();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "GET") {
-    res.status(200).json(items);
+    try {
+      const items = await prisma.item.findMany();
+      res.status(200).json(items);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
-
-export const items = [
-  {
-    name: "John",
-    age: "21",
-  },
-  {
-    name: "Elton",
-    age: "22",
-  },
-  {
-    name: "Elvis",
-    age: "21",
-  },
-  {
-    name: "Roger",
-    age: "21",
-  },
-  {
-    name: "Simon",
-    age: "21",
-  },
-  {
-    name: "Dean",
-    age: "21",
-  },
-];
