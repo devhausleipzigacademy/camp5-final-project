d96992d3ff2eb4a89f75b74fd8be7fa9e0688baf
inpatr <i.traub@posteo.de>
2022-05-16 16:51:55 +0200

    basics

---
+++ components/FilterButtons.tsx
@@ -0,0 +1,38 @@
+import { Item } from "@prisma/client";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const FilterButtons = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const router = useRouter();
+  const axios = require("axios");
+
+  async function clickHandler(selltype: string) {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/items?sellType=${selltype}`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+    useEffect(() => setItems, []);
+  }
+  return (
+    <div className="flex gap-2">
+      <button
+        className="bg-primary text-primary-text flex-grow py-2 rounded-sm"
+        onClick={clickHandler("FREE")}
+      >
+        Free
+      </button>
+      <button
+        className="bg-primary text-primary-text flex-grow py-2 rounded-sm"
+        onClick={clickHandler("SWAP")}
+      >
+        Swap
+      </button>
+    </div>
+  );
+};
+
+export default FilterButtons;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import FilterButtons from "../components/FilterButtons";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <FilterButtons />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
