2ad56dbb0c85f61ad172bbe96dfca340eaeb6e51
inpatr <i.traub@posteo.de>
2022-05-16 15:17:30 +0200

    implemented query

--- pages/api/items/index.ts
+++ pages/api/items/index.ts
@@ -1,14 +1,26 @@
-import { PrismaClient } from ".prisma/client";
+import { Item, PrismaClient } from ".prisma/client";
+import { SellType } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
+    const itemtype = req.query.sellType as SellType | undefined;
     try {
-      const items = await prisma.item.findMany();
+      let items: Item[] = [];
+      if (itemtype) {
+        items = await prisma.item.findMany({
+          where: {
+            sellType: itemtype,
+          },
+        });
+      } else {
+        items = await prisma.item.findMany();
+      }
       res.status(200).json(items);
     } catch (err) {
       console.log(err);
