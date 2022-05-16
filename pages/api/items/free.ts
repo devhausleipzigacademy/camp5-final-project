7749cdaf33e0904410cfbd0253112b2e7ef063df
Simon Börner <boernersimon@googlemail.com>
2022-05-16 14:46:50 +0200

    add swap and free routes

---
+++ pages/api/items/free.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from ".prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const items = await prisma.item.findMany({
+        where: { sellType: "FREE" },
+      });
+      res.status(200).json(items);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
