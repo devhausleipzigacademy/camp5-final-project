d3bc9de99629cff445e1917668f9270c050d00a2
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-17 12:21:28 +0200

    button, carousel, itemPage, done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import ProductPage from "./item";
+import { formatDistance, subDays } from 'date-fns'
+import { ProductProps } from "./item";
+// images testing data:
+const imgRes = [
+  "https://placeimg.com/640/360/any",
+  "https://placeimg.com/640/360/animals",
+  "https://placeimg.com/640/360/architecture",
+  "https://placeimg.com/640/360/nature",
+  "https://placeimg.com/640/360/people",
+  "https://placeimg.com/640/360/tech",
+];
+
+const ProductPropsTEST: ProductProps = {
+  imagesArray: imgRes,
+  title: "Camera",
+  offerType: 'Free',
+  owner: "Dean",
+  createdAt: formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true }),
+  distance: 12,
+  description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti porro exercitationem rerum assumenda atque perferendis ipsa, ullam maiores pariatur natus iste. Harum dolor dignissimos animi impedit praesentium, sapiente mollitia optio.",
+}
 
 const Home: NextPage = () => {
-  return <ProductPage />;
+  return (
+    <ProductPage {...ProductPropsTEST} />
+  );
 };
 
 export default Home;
--- pages/item.tsx
+++ pages/item.tsx
@@ -3,26 +3,17 @@ import Carousel from "../components/Carousel/Carousel";
 import ChatIcon from '../public/chat.svg';
 
 
-// images testing data:
-const imgRes = [
-  "https://placeimg.com/640/360/any",
-  "https://placeimg.com/640/360/animals",
-  "https://placeimg.com/640/360/architecture",
-  "https://placeimg.com/640/360/nature",
-  "https://placeimg.com/640/360/people",
-  "https://placeimg.com/640/360/tech",
-];
 
-interface ProductProps {
+export interface ProductProps {
   imagesArray: string[];
   title: string;
   offerType: "Free" | "Swap";
   owner: string;
-  createdAt: Date;
+  createdAt: string;
   distance: number;
   description: string;
 }
-export default function ProductPage() {
+export default function ProductPage({ imagesArray, title, offerType, owner, createdAt, distance, description }: ProductProps) {
   return (
     <>
       <div className="w-full h-1/4 py-4 bg-primary text-primary-text text-center">
@@ -30,37 +21,35 @@ export default function ProductPage() {
       </div>
       <div className="flex-col space-y-2">
         <div className="block w-full">
-          <Carousel imagesArray={imgRes} />
+          <Carousel imagesArray={imagesArray} />
         </div>
-        <div className="flex-col px-4 space-y-2">
+        <div className="flex-col px-4 space-y-2" >
           <div className="flex justify-between items-center">
             <div>
-              <p className="text-xl">Super Hype Camera</p>
+              <p className="text-xl">{title}</p>
             </div>
             <div>
-              <Button bgColor={"primary"} text={"Swap"} />
+              <Button bgColor={"primary"} text={offerType} />
             </div>
           </div>
           <div className="flex justify-between items-center">
             <div>
-              <p className="text-md">offered by john duo</p>
-              <p className="text-xs">distance 2 km</p>
-              <p className="text-xs">posted 2 days ago</p>
+              <p className="text-md"> {`Offered by ${owner}`} </p>
+              <p className="text-xs">{`distance ${distance} km`} </p>
+              <p className="text-xs">{`posted ${createdAt}`} </p>
             </div>
             <div>
               <button className="text-primary w-10"><ChatIcon /></button>
             </div>
           </div>
+
           <div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              iusto vitae porro accusamus repudiandae quis odit sunt tempora
-              vero, quidem veniam fugiat atque eligendi ipsa sequi autem? Ipsum,
-              tempore? Veritatis.
+              {description}
             </p>
-            <div className="min-h-full grow"></div>
           </div>
-          <div>
+
+          <div className="fixed bottom-3 right-5 left-5" >
             <Button bgColor={"secondary"} text={"Offer Trade"} py={2} />
           </div>
         </div>
