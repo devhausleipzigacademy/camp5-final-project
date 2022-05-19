c8da5f26e238420d682f782a60e549999153dac4
inpatr <i.traub@posteo.de>
2022-05-19 12:23:07 +0200

    we need to change everything

--- pages/api/items/index.ts
+++ pages/api/items/index.ts
@@ -1,4 +1,4 @@
-import { Item, PrismaClient } from ".prisma/client";
+import { Item, Location, PrismaClient } from ".prisma/client";
 import { SellType } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -12,15 +12,28 @@ export default async function handler(
     const itemtype = req.query.sellType as SellType | undefined;
     try {
       let items: Item[] = [];
+      let locations: Location[] = []
       if (itemtype) {
         items = await prisma.item.findMany({
           where: {
             sellType: itemtype,
           },
         });
+        locations = await prisma.location.findMany() 
       } else {
         items = await prisma.item.findMany();
+        locations = await prisma.location.findMany()
       }
+      items.forEach(
+        if(items[i].userId === ){
+          
+        }
+      )
+      // for (let i = 0; i <= items.length; i++) {
+        
+      //   if (items[i].userId === locations[i].userId)
+        
+      // }
       res.status(200).json(items);
     } catch (err) {
       console.log(err);
