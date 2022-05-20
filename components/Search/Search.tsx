f9fc9703f6ca8e56853477ecb16cf3438a3a205a
nort4n <trikkster@gmail.com>
2022-05-20 16:03:08 +0200

    fixed filter

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
-import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
-import { MapData, Feature, Geometry, Properties } from "../../utils/types";
+import { CheckIcon, SearchIcon } from "@heroicons/react/solid";
+import { Feature } from "../../utils/types";
 
 const people = [
   { id: 1, name: "Wade Cooper" },
@@ -20,16 +20,16 @@ export default function Search({ properties }: SearchProps) {
   const [selected, setSelected] = useState();
   const [query, setQuery] = useState("");
 
-  console.log(properties);
   const filteredItems =
     query === ""
-      ? properties
+      ? null
       : properties.filter((element) =>
           element.properties.title
             .toLowerCase()
             .replace(/\s+/g, "")
             .includes(query.toLowerCase().replace(/\s+/g, ""))
         );
+  console.log(filteredItems);
   return (
     <div className="fixed top-16 w-72">
       <Combobox value={selected} onChange={setSelected}>
@@ -40,7 +40,7 @@ export default function Search({ properties }: SearchProps) {
               onChange={(event) => setQuery(event.target.value)}
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <SelectorIcon
+              <SearchIcon
                 className="h-5 w-5 text-gray-400"
                 aria-hidden="true"
               />
@@ -49,12 +49,12 @@ export default function Search({ properties }: SearchProps) {
           <Transition
             as={Fragment}
             leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
+            leaveFrom="opacity-0"
             leaveTo="opacity-0"
             afterLeave={() => setQuery("")}
           >
             <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {!!filteredItems?.length && query !== "" ? (
+              {!filteredItems && query !== "" ? (
                 <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                   Nothing found.
                 </div>
@@ -64,8 +64,8 @@ export default function Search({ properties }: SearchProps) {
                   <Combobox.Option
                     key={index}
                     className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-teal-600 text-white" : "text-gray-900"
+                      `relative cursor-default select-none bg-BG py-2 pl-10 pr-4 ${
+                        active ? "bg-primary-text text-primary" : "bg-BG"
                       }`
                     }
                     value={element.properties.title}
