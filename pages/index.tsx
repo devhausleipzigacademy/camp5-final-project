20cbf51c21baa7f0ba4294de4528b8d405453644
inpatr <i.traub@posteo.de>
2022-05-20 14:46:17 +0200

    push again

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,16 @@ import Head from "next/head";
 import Image from "next/image";
 import React from "react";
 import { useEffect, useRef, useState } from "react";
+import { stores } from "../assets/data";
 import Header from "../components/Header/Header";
 import ItemDrawer from "../components/ItemDrawer/ItemDrawer";
 import ItemTypeButtons from "../components/ItemTypeButtons/itemTypeButtons";
+import ListingItem from "../components/ListingItem/ListingItem";
 // import styles from "../styles/Home.module.css";
 import Map from "../components/map";
 import SearchBar from "../components/SearchBar/searchbar";
 import useMap from "../hooks/useMap";
+import { Feature } from "../utils/types";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYXJvbjE4IiwiYSI6ImNsMzRibG9xYjB3ZjUzaW13d2s3bzVjcGkifQ.QGlBNyR336mJ2rFfFprAPg";
