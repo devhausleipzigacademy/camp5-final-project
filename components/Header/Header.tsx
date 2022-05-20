83b56d37372cf67cb19fb559a7a0eff332491ce0
Dean Wallflower <dean@deanwallflower.com>
2022-05-20 10:11:37 +0200

    add svg import to heaader module

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import BurgerMenu from "../../public/menu.svg";
 
 const Header = () => {
   const router = useRouter();
@@ -17,21 +18,7 @@ const Header = () => {
       <h3 className="text-base font-poppins">{pagename}</h3>
       {/* add burgermenu later */}
       <button className="fixed right-4">
-        <svg
-          width="32"
-          height="32"
-          viewBox="0 0 32 32"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M5.3335 8H26.6668M5.3335 13.3333H26.6668M5.3335 18.6667H26.6668M5.3335 24H26.6668"
-            stroke="#F7F5F6"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+        <BurgerMenu className="text-primary-text" width="32" />
       </button>
     </div>
   );
