bae1f8d8d81e95dc6f614f91ea052b1a3b7ed175
JonathanWagne <jonathan_wagner@hotmail.de>
2022-05-23 11:00:27 +0200

    ammend Readme

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ module.exports = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack"],
     });
     return config;
-  }
+  },
 };
