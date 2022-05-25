1585265b1c4acb501154cb3b776783462eaf4d2c
Dan McAtee <danielmcatee@me.com>
2022-05-25 10:18:52 +0200

    add complete mock data

--- assets/data.ts
+++ assets/data.ts
@@ -106,4 +106,169 @@ export const mockData = [
       address: "Kippenbergstraße 28, 04317 Leipzig",
     },
   },
+
+  {
+    user: {
+      firstname: "Petra",
+      lastname: "Hurz",
+      email: "petra.hurz@email.de",
+      passwordHash: "ögfkgf",
+      passwordSalt: "asfhfd",
+      profilePicture:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687",
+      rating: 2,
+      favorite: ["5"],
+    },
+    items: [
+      {
+        title: "Glasses",
+        images: [
+          "https://images.unsplash.com/photo-1534844978-b859e5a09ad6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074",
+        ],
+        description: "One glass is broken, but otherwise good",
+        sellType: SellType.SWAP,
+      },
+    ],
+    category: {
+      title: "Glasses",
+      description: "Have you seen my glasses?",
+    },
+    location: {
+      lat: 51.3357937256552,
+      lon: 12.406558419972988,
+      address: "Täubchenweg 86, 04317 Leipzig",
+    },
+  },
+
+  {
+    user: {
+      firstname: "Simone",
+      lastname: "Malz",
+      email: "simone.malz@email.de",
+      passwordHash: "kogspdfosd",
+      passwordSalt: "öojgpodsjgsd",
+      profilePicture:
+        "https://images.unsplash.com/photo-1619895862022-09114b41f16f?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+      rating: 4,
+      favorite: ["2"],
+    },
+    items: [
+      {
+        title: "Tie",
+        images: [
+          "https://images.unsplash.com/photo-1589756823695-278bc923f962?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687",
+        ],
+        description: "nice animals, only used at wedding",
+        sellType: SellType.SWAP,
+      },
+    ],
+    category: {
+      title: "Ties",
+      description: "For the serious days",
+    },
+    location: {
+      lat: 51.33959900523878,
+      lon: 12.40713727722273,
+      address: "Lilienstraße 39a, 04315 Leipzig",
+    },
+  },
+
+  {
+    user: {
+      firstname: "Ines",
+      lastname: "Schwarz",
+      email: "ines.schwarz@email.de",
+      passwordHash: "gsdgds",
+      passwordSalt: "AFHFD",
+      profilePicture:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+      rating: 5,
+      favorite: ["1"],
+    },
+    items: [
+      {
+        title: "Blouse",
+        images: [
+          "https://images.unsplash.com/photo-1584030373081-f37b7bb4fa8e?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=691",
+        ],
+        description: "white, classic",
+        sellType: SellType.FREE,
+      },
+    ],
+    category: {
+      title: "Blouse",
+      description: "We don't know anything about Blouses",
+    },
+    location: {
+      lat: 51.33799145756473,
+      lon: 12.401286770922855,
+      address: "Heinrichstraße 16, 04317 Leipzig",
+    },
+  },
+
+  {
+    user: {
+      firstname: "Aylin",
+      lastname: "Yilmaz",
+      email: "aylin.yilmaz@email.de",
+      passwordHash: "khggb",
+      passwordSalt: "sgjzrj",
+      profilePicture:
+        "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687",
+      rating: 2,
+      favorite: ["1"],
+    },
+    items: [
+      {
+        title: "Sweatshirt",
+        images: [
+          "https://images.unsplash.com/photo-1572495673508-62a6b369c380?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687",
+        ],
+        description: "not in a really good condition, black",
+        sellType: SellType.FREE,
+      },
+    ],
+    category: {
+      title: "Sweatshirt",
+      description: "Let's sweat!",
+    },
+    location: {
+      lat: 51.3413036318854,
+      lon: 12.406251931287448,
+      address: "Hermann-Liebmann-Straße 27, 04315 Leipzig",
+    },
+  },
+
+  {
+    user: {
+      firstname: "Alistair",
+      lastname: "Fisher",
+      email: "alistair.fisher@email.de",
+      passwordHash: "gdfgdf",
+      passwordSalt: "gdgdf",
+      profilePicture:
+        "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+      rating: 2,
+      favorite: ["4"],
+    },
+    items: [
+      {
+        title: "Suit",
+        images: [
+          "https://images.unsplash.com/photo-1594938298603-c8148c4dae35?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880",
+        ],
+        description: "suit including shirt, vest, jacket, trousers and tie",
+        sellType: SellType.FREE,
+      },
+    ],
+    category: {
+      title: "Suits",
+      description: "For the serious days",
+    },
+    location: {
+      lat: 51.339674129638844,
+      lon: 12.410080043100086,
+      address: "Bernhardstraße 27, 04315 Leipzig",
+    },
+  },
 ];
