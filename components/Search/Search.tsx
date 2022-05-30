8ea886b00661f46c662f507813eb74fdf742fdce
nort4n <trikkster@gmail.com>
2022-05-30 15:25:23 +0200

    styling

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -23,69 +23,64 @@ export default function Search({ properties }: SearchProps) {
   console.log(properties);
   return (
     <div className="flex px-2 mt-2">
-      <div className="w-full h-12 rounded-md bg-BG text-center">
+      <div className="w-full h-full rounded-md border-primary border-2 text-center">
         <Combobox value={selected} onChange={setSelected}>
-          <div>
-            <div className="w-full cursor-default overflow-hidden bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 sm:text-sm">
-              <Combobox.Input
-                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                onChange={(event) => setQuery(event.target.value)}
-              />
-              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                <SearchIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </Combobox.Button>
-            </div>
-            <Transition
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-0"
-              leaveTo="opacity-0"
-              afterLeave={() => setQuery("")}
-            >
-              <Combobox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg focus:outline-none sm:text-sm">
-                {!filteredItems && query !== "" ? (
-                  <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                    Nothing found.
-                  </div>
-                ) : (
-                  !!filteredItems?.length &&
-                  filteredItems.map((element, index) => (
-                    <Combobox.Option
-                      key={index}
-                      className={({ active }) =>
-                        `relative cursor-default select-none bg-BG py-2 pl-10 pr-4 ${
-                          active ? "bg-primary-text text-primary" : "bg-BG"
-                        }`
-                      }
-                      value={element.properties.title}
-                    >
-                      {({ selected, active }) => (
-                        <>
-                          <span
-                            className={`block truncate ${
-                              selected ? "font-medium" : "font-normal"
-                            }`}
-                          >
-                            {element.properties.title}
-                          </span>
-                          {selected ? (
-                            <span
-                              className={`absolute inset-y-0 left-0 flex items-start pl-3 ${
-                                active ? "text-white" : "text-teal-600"
-                              }`}
-                            ></span>
-                          ) : null}
-                        </>
-                      )}
-                    </Combobox.Option>
-                  ))
-                )}
-              </Combobox.Options>
-            </Transition>
+          <div className="relative w-full cursor-default overflow-hidden bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 text-sm leading-5 text-BG-text focus:ring-0"
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <SearchIcon className="h-5 w-5 text-BG-text" aria-hidden="true" />
+            </Combobox.Button>
           </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-0"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options className="z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg focus:outline-none sm:text-sm">
+              {!filteredItems && query !== "" ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-BG-text">
+                  Nothing found.
+                </div>
+              ) : (
+                !!filteredItems?.length &&
+                filteredItems.map((element, index) => (
+                  <Combobox.Option
+                    key={index}
+                    className={({ active }) =>
+                      `relative cursor-default select-none bg-BG py-2 pl-10 pr-4 ${
+                        active ? "bg-primary-text text-primary" : "bg-BG"
+                      }`
+                    }
+                    value={element.properties.title}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {element.properties.title}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-start pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
         </Combobox>
       </div>
     </div>
