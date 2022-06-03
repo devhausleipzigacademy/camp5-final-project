632d6dd54606e37ac929f86e483fb499fded8b14
Simon Börner <boernersimon@googlemail.com>
2022-06-03 15:49:32 +0200

    add useritems page

--- components/UserListItem/UserListItem.tsx
+++ components/UserListItem/UserListItem.tsx
@@ -10,11 +10,27 @@ type Props = {
   itemPosted: string;
   itemRequests: number;
   itemGone: boolean;
+  itemRecipient: string;
 };
 
 export const UserListItem = (item: Props) => {
+  let requestStatus;
+  if (item.itemGone) {
+    requestStatus = <div>Item is gone</div>;
+  } else if (item.itemRequests > 0 && item.itemType === "SWAP") {
+    requestStatus = <div>{item.itemRequests} pending swap requests</div>;
+  } else if (!item.itemRequests && item.itemType === "SWAP") {
+    requestStatus = <div>No swap requests</div>;
+  } else if (item.itemRequests > 0 && item.itemType === "FREE") {
+    requestStatus = <div>{item.itemRequests} pending requests</div>;
+  } else if (!item.itemRequests && item.itemType === "FREE") {
+    requestStatus = <div>No requests</div>;
+  }
+
   return (
-    <div className="flex p-4 gap-3">
+    <div
+      className={item.itemGone ? "flex p-4 gap-3 opacity-50" : "flex p-4 gap-3"}
+    >
       <Image
         src={item.itemImage}
         height={50}
@@ -22,10 +38,22 @@ export const UserListItem = (item: Props) => {
         objectFit="cover"
         layout="intrinsic"
       />
+
       <div>
-        <div>{item.itemTitle}</div>
-        <div>{`Posted On ${item.itemPosted}`}</div>
-        <div></div>
+        <div className={item.itemGone ? "line-through" : "no-underline"}>
+          {item.itemTitle}
+        </div>
+        <div>{`Posted on ${item.itemPosted}`}</div>
+        {item.itemGone && item.itemType === "SWAP" ? (
+          requestStatus
+        ) : (
+          <div>Swapped with {item.itemRecipient}</div>
+        )}
+        {item.itemGone && item.itemType === "FREE" ? (
+          requestStatus
+        ) : (
+          <div>Gifted to {item.itemRecipient}</div>
+        )}
       </div>
     </div>
   );
--- pages/useritems.tsx
+++ pages/useritems.tsx
@@ -7,11 +7,14 @@ const UserItems = () => {
     <div className="pt-20">
       <UserListItem
         itemImage={
-          "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
+          "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
         }
         itemType={"SWAP"}
-        itemTitle={"Gift Gift Gift"}
+        itemTitle={"Boots"}
         itemPosted="03.06.2022"
+        itemRequests={0}
+        itemGone={false}
+        itemRecipient={"Jonathan"}
       />
     </div>
   );
