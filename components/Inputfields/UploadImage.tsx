1093ae0d3addecc988023e7623aae16f3feca859
inpatr <i.traub@posteo.de>
2022-06-07 11:26:27 +0200

    created component

--- components/Inputfields/UploadImage.tsx
+++ components/Inputfields/UploadImage.tsx
@@ -1,17 +1,55 @@
+import { PlusCircleIcon } from "@heroicons/react/solid";
+import { FileContent, FileError } from "use-file-picker/dist/interfaces";
+
 {
   /* Upload Multiple Files */
 }
 
-const UploadImage = () => {
+interface Props {
+  errors: FileError[];
+  filesContent: FileContent[];
+  openFileSelector: () => void;
+}
+
+const UploadImage = ({ errors, filesContent, openFileSelector }: Props) => {
   return (
-    <div className="mt-6">
-      <p>Upload Images and Videos here</p>
-      <input
-        className="px-3 py-2 block w-full text-sm text-gray-900 bg-BG rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-BG dark:border-gray-600 dark:placeholder-gray-400"
-        id="multiple_files"
-        type="file"
-        multiple
-      />
+    <div className="w-full">
+      {errors.length ? (
+        errors.map((error, index) => (
+          <div className="w-full rounded-md bg-primary bg-opacity-20 text-primary text-opacity-40 px-3 py-2 flex flex-row flex-grow justify-between items-center sm:text-sm">
+            <div key={index} className="flex bg-error text-error-text">
+              <p>{error.name}</p>
+              <p>INVALID PIC</p>
+              <br />
+            </div>
+            <button onClick={() => openFileSelector()}>
+              <PlusCircleIcon className="text-primary h-8" />
+            </button>
+          </div>
+        ))
+      ) : filesContent.length ? (
+        <div>
+          {filesContent.map((file, index) => (
+            <div key={index}>
+              <div className="w-full rounded-md bg-primary bg-opacity-20 text-primary text-opacity-40 px-3 py-2 flex flex-row justify-between items-center sm:text-sm">
+                <p className="font-normal">{file.name}</p>
+                <button onClick={() => openFileSelector()}>
+                  <PlusCircleIcon className="text-primary h-8" />
+                </button>
+              </div>
+              <img alt={file.name} src={file.content}></img>
+              <br />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="w-full rounded-md bg-primary bg-opacity-20 text-primary text-opacity-40 px-3 py-2 flex flex-row justify-between items-center sm:text-sm">
+          <p className="font-normal">Photo </p>
+          <button onClick={() => openFileSelector()}>
+            <PlusCircleIcon className="text-primary h-8" />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -184,44 +184,12 @@ const UploadPage: NextPage = () => {
           onChange={(event) => setDescription(event.target.value)}
         />
         {/* ---------------------- UPLOAD ------------------------- */}
-        <div className="w-full">
-          {errors.length ? (
-            errors.map((error, index) => (
-              <div className="w-full rounded-md bg-primary bg-opacity-20 text-primary text-opacity-40 px-3 py-2 flex flex-row flex-grow justify-between items-center sm:text-sm">
-                <div key={index} className="flex bg-error text-error-text">
-                  <p>{error.name}</p>
-                  <p>INVALID PIC</p>
-                  <br />
-                </div>
-                <button onClick={() => openFileSelector()}>
-                  <PlusCircleIcon className="text-primary h-8" />
-                </button>
-              </div>
-            ))
-          ) : filesContent.length ? (
-            <div>
-              {filesContent.map((file, index) => (
-                <div key={index}>
-                  <div className="w-full rounded-md bg-primary bg-opacity-20 text-primary text-opacity-40 px-3 py-2 flex flex-row justify-between items-center sm:text-sm">
-                    <p className="font-normal">{file.name}</p>
-                    <button onClick={() => openFileSelector()}>
-                      <PlusCircleIcon className="text-primary h-8" />
-                    </button>
-                  </div>
-                  <img alt={file.name} src={file.content}></img>
-                  <br />
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div className="w-full rounded-md bg-primary bg-opacity-20 text-primary text-opacity-40 px-3 py-2 flex flex-row justify-between items-center sm:text-sm">
-              <p className="font-normal">Photo </p>
-              <button onClick={() => openFileSelector()}>
-                <PlusCircleIcon className="text-primary h-8" />
-              </button>
-            </div>
-          )}
-        </div>
+        <UploadImage
+          errors={errors}
+          filesContent={filesContent}
+          openFileSelector={openFileSelector}
+        />
+
         {/* ---------------------- CHECKBOXES ------------------------- */}
 
         <div className="flex flex-row py-3 ">
