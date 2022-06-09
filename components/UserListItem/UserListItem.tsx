beadbdc12f2e1be811faed53e36244418e5e3cc8
nort4n <trikkster@gmail.com>
2022-06-09 18:12:36 +0200

    useritemlist

--- components/UserListItem/UserListItem.tsx
+++ components/UserListItem/UserListItem.tsx
@@ -16,19 +16,6 @@ type Props = {
 };
 
 export const UserListItem = (item: Props) => {
-    // let requestStatus;
-    // if (item.itemGone) {
-    //   requestStatus = <div>Item is gone</div>;
-    // } else if (item.itemRequests > 0 && item.itemType === "SWAP") {
-    //   requestStatus = <div>{item.itemRequests} pending swap requests</div>;
-    // } else if (!item.itemRequests && item.itemType === "SWAP") {
-    //   requestStatus = <div>No swap requests</div>;
-    // } else if (item.itemRequests > 0 && item.itemType === "FREE") {
-    //   requestStatus = <div>{item.itemRequests} pending requests</div>;
-    // } else if (!item.itemRequests && item.itemType === "FREE") {
-    //   requestStatus = <div>No requests</div>;
-    // }
-
     return (
         <div
             className={
--- pages/useritems.tsx
+++ pages/useritems.tsx
@@ -1,49 +1,54 @@
 import { UserListItem } from "../components/UserListItem/UserListItem";
-import React from "react";
-import { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+
 import CreateItemButton from "../components/Button/CreateItemButton/CreateItemButton";
+import { Item } from "../utils/types";
+
+const [items, setitems] = useState<Item[]>([]);
+
+useEffect(() => {}, []);
 
 const UserItems = () => {
-  return (
-    <div className="pt-20">
-      <UserListItem
-        itemImage={
-          "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
-        }
-        itemType={"FREE"}
-        itemTitle={"Boots"}
-        itemPosted="03.06.2022"
-        itemRequests={5}
-        itemGone={true}
-        itemRecipient={"Jonathan"}
-      />
-      <UserListItem
-        itemImage={
-          "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
-        }
-        itemType={"FREE"}
-        itemTitle={"Boots"}
-        itemPosted="03.06.2022"
-        itemRequests={5}
-        itemGone={false}
-        itemRecipient={"Jonathan"}
-      />
-      <UserListItem
-        itemImage={
-          "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
-        }
-        itemType={"FREE"}
-        itemTitle={"Boots"}
-        itemPosted="03.06.2022"
-        itemRequests={0}
-        itemGone={false}
-        itemRecipient={"Jonathan"}
-      />
-      <div>
-        <CreateItemButton />
-      </div>
-    </div>
-  );
+    return (
+        <div className="pt-20">
+            <UserListItem
+                itemImage={
+                    "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
+                }
+                itemType={"FREE"}
+                itemTitle={"Boots"}
+                itemPosted="03.06.2022"
+                itemRequests={5}
+                itemGone={true}
+                itemRecipient={"Jonathan"}
+            />
+            <UserListItem
+                itemImage={
+                    "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
+                }
+                itemType={"FREE"}
+                itemTitle={"Boots"}
+                itemPosted="03.06.2022"
+                itemRequests={5}
+                itemGone={false}
+                itemRecipient={"Jonathan"}
+            />
+            <UserListItem
+                itemImage={
+                    "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
+                }
+                itemType={"FREE"}
+                itemTitle={"Boots"}
+                itemPosted="03.06.2022"
+                itemRequests={0}
+                itemGone={false}
+                itemRecipient={"Jonathan"}
+            />
+            <div>
+                <CreateItemButton />
+            </div>
+        </div>
+    );
 };
 
 export default UserItems;
