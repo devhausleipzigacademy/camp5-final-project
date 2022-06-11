d065c21305de07fd07d3a4ad1596ed6fc90978c7
nort4n <trikkster@gmail.com>
2022-06-11 12:13:21 +0200

    deleted unused imports

--- pages/api/item/index.ts
+++ pages/api/item/index.ts
@@ -1,25 +1,24 @@
 import { Item, PrismaClient } from ".prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { CoffeeMachineItem } from "../../../prisma/models";
 
 const prisma = new PrismaClient();
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    try {
-      let item: Item = await prisma.item.create({
-        data: {
-          ...req.body,
-        },
-      });
-      res.status(200).json(item);
-      res.end();
-    } catch (err) {
-      console.log(err);
-      res.status(500).end();
+    if (req.method === "POST") {
+        try {
+            let item: Item = await prisma.item.create({
+                data: {
+                    ...req.body,
+                },
+            });
+            res.status(200).json(item);
+            res.end();
+        } catch (err) {
+            console.log(err);
+            res.status(500).end();
+        }
     }
-  }
 }
