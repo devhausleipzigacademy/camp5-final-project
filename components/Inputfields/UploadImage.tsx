2cb27f1545699c789a5debf3832864ebed32f964
inpatr <i.traub@posteo.de>
2022-06-13 11:49:43 +0200

    can delete images

--- components/Inputfields/UploadImage.tsx
+++ components/Inputfields/UploadImage.tsx
@@ -1,4 +1,5 @@
 import { PlusCircleIcon, TrashIcon } from "@heroicons/react/solid";
+import { MouseEventHandler, useEffect, useState } from "react";
 import { FileContent, FileError } from "use-file-picker/dist/interfaces";
 
 {
@@ -9,12 +10,17 @@ interface Props {
   errors: FileError[];
   filesContent: FileContent[];
   openFileSelector: () => void;
+  clear: () => void;
 }
 
-const UploadImage = ({ errors, filesContent, openFileSelector }: Props) => {
-  function deleteHandler(){
-    filesContent.find((file) => file.name === )
-  }
+const UploadImage = ({
+  errors,
+  filesContent,
+  openFileSelector,
+  clear,
+}: Props) => {
+  // const [images, setImages] = useState()
+
   return (
     <div className="w-full">
       {errors.length ? (
@@ -39,20 +45,13 @@ const UploadImage = ({ errors, filesContent, openFileSelector }: Props) => {
             <p className="font-normal">
               you've uploaded {filesContent.length} image/s
             </p>
-            <button onClick={() => openFileSelector()}>
-              <PlusCircleIcon className="text-primary h-8" />
+            <button onClick={clear}>
+              <TrashIcon className="text-primary h-6" />
             </button>
           </div>
           {filesContent.map((file, index) => (
             <div key={index}>
-              <div className="flex justify-between py-4 w-full">
-                <p className="font-normal text-BG-text opacity-80">
-                  {file.name}
-                </p>
-                <button onClick={() => {}}>
-                  <TrashIcon className="text-primary h-6" />
-                </button>
-              </div>
+              <p className="font-normal text-BG-text opacity-80">{file.name}</p>
               <img id={file.name} alt={file.name} src={file.content}></img>
               <br />
             </div>
