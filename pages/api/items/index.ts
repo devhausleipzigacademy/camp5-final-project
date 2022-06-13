154b6dfb0d2d569cdaeef5ddad27df82614d3005
nort4n <trikkster@gmail.com>
2022-06-13 11:18:02 +0200

    endpoint for finding items in one category

--- pages/api/items/index.ts
+++ pages/api/items/index.ts
@@ -1,29 +1,44 @@
 import { Item, PrismaClient } from ".prisma/client";
 import { SellType } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { MockKitchenCategories } from "../../../utils/types";
 
 const prisma = new PrismaClient();
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    const itemtype = req.query.sellType as SellType | undefined;
-    try {
-      let items: Item[] = [];
-      if (itemtype) {
-        items = await prisma.item.findMany({
-          where: {
-            sellType: itemtype,
-          },
-        });
-      } else {
-        items = await prisma.item.findMany();
-      }
-      res.status(200).json(items);
-    } catch (err) {
-      console.log(err);
+    if (req.method === "GET") {
+        const itemtype = req.query.sellType as SellType | undefined;
+        const domainTitle = req.query.domainTitle as string | undefined;
+        const categoryTitle = req.query.categoryTitle;
+        try {
+            let items: Item[] = [];
+            if (itemtype) {
+                items = await prisma.item.findMany({
+                    where: {
+                        sellType: itemtype,
+                    },
+                });
+            }
+            if (categoryTitle) {
+                items = await prisma.item.findMany({
+                    where: {
+                        categoryTitle: categoryTitle,
+                    },
+                });
+                // }
+                // if (domainTitle) {
+                //     items = await prisma.item.findMany({
+                //         where: {},
+                //     });
+            } else {
+                items = await prisma.item.findMany();
+            }
+            res.status(200).json(items);
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
 }
