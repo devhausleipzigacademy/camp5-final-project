f046adbb39dcb2a97eab68e837f303069cfa2f15
nort4n <trikkster@gmail.com>
2022-06-22 11:54:48 +0200

    changed mapdata to include item class in order to filter in search component

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,10 +1,10 @@
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import {
-  CheckIcon,
-  SearchIcon,
-  GiftIcon,
-  SwitchVerticalIcon,
+    CheckIcon,
+    SearchIcon,
+    GiftIcon,
+    SwitchVerticalIcon,
 } from "@heroicons/react/solid";
 import { Feature } from "../../utils/types";
 import createPopUp from "../../utils/createPopUp";
@@ -15,103 +15,187 @@ import { useMapStore } from "../../stores/mapStore";
 import { useLocationStore } from "../../stores/locationStore";
 import { useRouter } from "next/router";
 type SearchProps = {
-  properties: Feature[];
+    properties: Feature[];
 };
 
 export default function Search({ properties }: SearchProps) {
-  const router = useRouter();
-  const [selected, setSelected] = useState();
-  const [query, setQuery] = useState("");
-  const { mapRef } = useMapStore();
-  const { location } = useLocationStore();
+    const router = useRouter();
+    const [selected, setSelected] = useState();
+    const [query, setQuery] = useState("");
+    const { mapRef } = useMapStore();
+    const { location } = useLocationStore();
 
-  const filteredItems =
-    query === ""
-      ? null
-      : properties.filter((element) =>
-          element.properties.title
-            .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+    const filteredItems =
+        query === ""
+            ? null
+            : properties.filter((element) =>
+                  element.properties.title
+                      .toLowerCase()
+                      .replace(/\s+/g, "")
+                      .includes(query.toLowerCase().replace(/\s+/g, ""))
+              );
+    const filteredCategories =
+        query === ""
+            ? null
+            : properties.filter((element) =>
+                  element.properties.class
+                      .toLowerCase()
+                      .replace(/\s+/g, "")
+                      .includes(query.toLowerCase().replace(/\s+/g, ""))
+              );
 
-  return (
-    <div className="flex px-2 mt-2">
-      <div className="w-full h-full rounded-md border-primary border-2 text-center">
-        <Combobox value={selected} onChange={setSelected}>
-          <div className="relative w-full cursor-default overflow-hidden bg-white text-left">
-            <Combobox.Input
-              className="pl-2 w-10/12 focus:outline-none border-none focus:border-none rounded-md py-2 text-sm text-BG-text"
-              onChange={(event) => setQuery(event.target.value)}
-            />
-            <Combobox.Button className="absolute inset-y-0 right-2 flex items-center opacity-100">
-              <SearchIcon className="h-5 w-5 text-BG-text" aria-hidden="true" />
-            </Combobox.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-0"
-            leaveTo="opacity-0"
-            afterLeave={() => setQuery("")}
-          >
-            <Combobox.Options className="absolute z-50 mt-1 max-h-60 w-[calc(100vw-16px)] overflow-auto rounded-md bg-white text-base shadow-lg sm:text-sm">
-              {filteredItems?.length === 0 && query.length > 0 ? (
-                <Combobox.Option
-                  value=""
-                  key=""
-                  className="flex space-x-3 relative cursor-default select-none bg-BG"
-                >
-                  <span className="relative py-2 px-4 text-BG-text overflow-auto">
-                    Nothing found.
-                  </span>
-                </Combobox.Option>
-              ) : (
-                !!filteredItems?.length &&
-                filteredItems.map((element, index) => (
-                  <Combobox.Option
-                    key={index}
-                    className={({ active }) =>
-                      `flex space-x-3 relative cursor-default select-none ${
-                        active ? "bg-BG text-primary" : "bg-BG"
-                      }`
-                    }
-                    value={element.properties.title}
-                    onClick={() => {
-                      createPopUp(element, location, mapRef, router);
-                      setTimeout(() => flyToStore(element, mapRef), 300);
-                    }}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        {element.type === "FREE" ? (
-                          <GiftIcon className="w-6 h-6 ml-1 flex justify-center" />
-                        ) : (
-                          <SwitchVerticalIcon className="w-6 h-6 ml-1 flex justify-center" />
-                        )}
-                        <span
-                          className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
-                          }`}
-                        >
-                          {element.properties.title}
-                        </span>
-                        {selected ? (
-                          <span
-                            className={`absolute inset-y-0 left-0 flex items-start pl-3 ${
-                              active ? "text-white" : "text-BG-text"
-                            }`}
-                          ></span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
-        </Combobox>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex px-2 mt-2">
+            <div className="w-full h-full rounded-md border-primary border-2 text-center">
+                <Combobox value={selected} onChange={setSelected}>
+                    <div className="relative w-full cursor-default overflow-hidden bg-white text-left">
+                        <Combobox.Input
+                            className="pl-2 w-10/12 focus:outline-none border-none focus:border-none rounded-md py-2 text-sm text-BG-text"
+                            onChange={(event) => setQuery(event.target.value)}
+                        />
+                        <Combobox.Button className="absolute inset-y-0 right-2 flex items-center opacity-100">
+                            <SearchIcon
+                                className="h-5 w-5 text-BG-text"
+                                aria-hidden="true"
+                            />
+                        </Combobox.Button>
+                    </div>
+                    <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-0"
+                        leaveTo="opacity-0"
+                        afterLeave={() => setQuery("")}
+                    >
+                        <Combobox.Options className="absolute z-50 mt-1 max-h-60 w-[calc(100vw-16px)] overflow-auto rounded-md bg-white text-base shadow-lg sm:text-sm">
+                            {filteredItems?.length === 0 &&
+                            query.length > 0 &&
+                            filteredCategories ? (
+                                <Combobox.Option
+                                    value=""
+                                    key=""
+                                    className="flex space-x-3 relative cursor-default select-none bg-BG"
+                                >
+                                    <span className="relative py-2 px-4 text-BG-text overflow-auto">
+                                        Nothing found.
+                                    </span>
+                                </Combobox.Option>
+                            ) : (
+                                !!filteredItems?.length &&
+                                !!filteredCategories?.length &&
+                                filteredItems.map((element, index) => (
+                                    <Combobox.Option
+                                        key={index}
+                                        className={({ active }) =>
+                                            `flex space-x-3 relative cursor-default select-none ${
+                                                active
+                                                    ? "bg-BG text-primary"
+                                                    : "bg-BG"
+                                            }`
+                                        }
+                                        value={element.properties.title}
+                                        onClick={() => {
+                                            createPopUp(
+                                                element,
+                                                location,
+                                                mapRef,
+                                                router
+                                            );
+                                            setTimeout(
+                                                () =>
+                                                    flyToStore(element, mapRef),
+                                                300
+                                            );
+                                        }}
+                                    >
+                                        {({ selected, active }) => (
+                                            <>
+                                                {element.type === "FREE" ? (
+                                                    <GiftIcon className="w-6 h-6 ml-1 flex justify-center" />
+                                                ) : (
+                                                    <SwitchVerticalIcon className="w-6 h-6 ml-1 flex justify-center" />
+                                                )}
+                                                <span
+                                                    className={`block truncate ${
+                                                        selected
+                                                            ? "font-medium"
+                                                            : "font-normal"
+                                                    }`}
+                                                >
+                                                    {element.properties.title}
+                                                </span>
+                                                {selected ? (
+                                                    <span
+                                                        className={`absolute inset-y-0 left-0 flex items-start pl-3 ${
+                                                            active
+                                                                ? "text-white"
+                                                                : "text-BG-text"
+                                                        }`}
+                                                    ></span>
+                                                ) : null}
+                                            </>
+                                        )}
+                                    </Combobox.Option>
+                                )) &&
+                                filteredCategories.map((element, index) => (
+                                    <Combobox.Option
+                                        key={index}
+                                        className={({ active }) =>
+                                            `flex space-x-3 relative cursor-default select-none ${
+                                                active
+                                                    ? "bg-BG text-primary"
+                                                    : "bg-BG"
+                                            }`
+                                        }
+                                        value={element.properties.title}
+                                        onClick={() => {
+                                            createPopUp(
+                                                element,
+                                                location,
+                                                mapRef,
+                                                router
+                                            );
+                                            setTimeout(
+                                                () =>
+                                                    flyToStore(element, mapRef),
+                                                300
+                                            );
+                                        }}
+                                    >
+                                        {({ selected, active }) => (
+                                            <>
+                                                {element.type === "FREE" ? (
+                                                    <GiftIcon className="w-6 h-6 ml-1 flex justify-center" />
+                                                ) : (
+                                                    <SwitchVerticalIcon className="w-6 h-6 ml-1 flex justify-center" />
+                                                )}
+                                                <span
+                                                    className={`block truncate ${
+                                                        selected
+                                                            ? "font-medium"
+                                                            : "font-normal"
+                                                    }`}
+                                                >
+                                                    {element.properties.title}
+                                                </span>
+                                                {selected ? (
+                                                    <span
+                                                        className={`absolute inset-y-0 left-0 flex items-start pl-3 ${
+                                                            active
+                                                                ? "text-white"
+                                                                : "text-BG-text"
+                                                        }`}
+                                                    ></span>
+                                                ) : null}
+                                            </>
+                                        )}
+                                    </Combobox.Option>
+                                ))
+                            )}
+                        </Combobox.Options>
+                    </Transition>
+                </Combobox>
+            </div>
+        </div>
+    );
 }
--- pages/api/map/index.ts
+++ pages/api/map/index.ts
@@ -1,68 +1,69 @@
 import { Item, Location, PrismaClient, User } from ".prisma/client";
 import { SellType } from "@prisma/client";
-import { el } from "date-fns/locale";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Feature, MapData } from "../../../utils/types";
 
 const prisma = new PrismaClient();
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    const itemtype = req.query.sellType as SellType | undefined;
-    try {
-      let items: Item[] = [];
-      let locations: Location[] = [];
-      let users: User[] = [];
+    if (req.method === "GET") {
+        const itemtype = req.query.sellType as SellType | undefined;
+        try {
+            let items: Item[] = [];
+            let locations: Location[] = [];
+            let users: User[] = [];
 
-      if (itemtype) {
-        items = await prisma.item.findMany({
-          where: {
-            sellType: itemtype,
-          },
-        });
-        locations = await prisma.location.findMany();
-        users = await prisma.user.findMany();
-      } else {
-        items = await prisma.item.findMany();
-        locations = await prisma.location.findMany();
-        users = await prisma.user.findMany();
-      }
-      //define a response object
-      const data: MapData = {
-        type: "FeatureCollection",
-        features: [],
-      };
-      //"fill" response object
-      items.forEach((item) => {
-        locations.forEach((location) => {
-          users.forEach((user) => {
-            if (
-              item.userId === location.userId &&
-              item.userId === user.identifier
-            ) {
-              const featureObject: Feature = {
-                type: item.sellType,
-                geometry: {
-                  type: "Point",
-                  coordinates: [location.lon, location.lat],
-                },
-                properties: {
-                  title: item.title,
-                  id: item.identifier,
-                  owner: user.firstname,
-                },
-              };
-              data.features.push(featureObject);
+            if (itemtype) {
+                items = await prisma.item.findMany({
+                    where: {
+                        sellType: itemtype,
+                    },
+                });
+                locations = await prisma.location.findMany();
+                users = await prisma.user.findMany();
+            } else {
+                items = await prisma.item.findMany();
+                locations = await prisma.location.findMany();
+                users = await prisma.user.findMany();
             }
-          });
-        });
-      });
-      res.status(200).json(data);
-    } catch (err) {
-      console.log(err);
+            //define a response object
+            const data: MapData = {
+                type: "FeatureCollection",
+                features: [],
+            };
+            //"fill" response object
+            items.forEach((item) => {
+                locations.forEach((location) => {
+                    users.forEach((user) => {
+                        if (
+                            item.userId === location.userId &&
+                            item.userId === user.identifier
+                        ) {
+                            const featureObject: Feature = {
+                                type: item.sellType,
+                                geometry: {
+                                    type: "Point",
+                                    coordinates: [location.lon, location.lat],
+                                },
+                                properties: {
+                                    title: item.title,
+                                    id: item.identifier,
+                                    owner: user.firstname,
+                                    class: item.class,
+                                },
+                            };
+                            data.features.push(featureObject);
+                        }
+                    });
+                });
+            });
+            res.status(200).json(data);
+        } catch (err) {
+            console.log(err);
+        }
     }
-  }
 }
