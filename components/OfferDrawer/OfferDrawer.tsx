1b8bb79ad1b500bbaeb7afda27352cc1b78636bf
inpatr <i.traub@posteo.de>
2022-06-23 12:50:32 +0200

    my items fetch works!

--- components/OfferDrawer/OfferDrawer.tsx
+++ components/OfferDrawer/OfferDrawer.tsx
@@ -65,47 +65,51 @@ const OfferDrawer = ({ show = false }: OfferDrawerProps) => {
     console.log(selID);
   };
 
-  return (
-    <>
-      <div
-        className={clsx(
-          show
-            ? "absolute h-[calc(100vh-92px)] w-1/3 top-[64px] bg-BG shadow-md rounded-r-md z-10 flex flex-col justify-between px-2 py-4 transition-transform duration-150 ease-out translate-x-0"
-            : "absolute h-[calc(100vh-92px)] w-1/3 top-[64px] bg-BG shadow-md z-10 flex flex-col justify-between px-2 py-4 transition-transform duration-150 ease-out -translate-x-full"
-        )}
-      >
-        <div className="flex flex-col space-y-2 justify-between text-secondary-text">
-          {userItems.length === 0 && (
-            <div className="flex text-center items-center w-full h-[73.5vh] rounded-md">
-              <Spinner />
-            </div>
+  if (!userItems) {
+    return <>you need to upload items</>;
+  } else {
+    return (
+      <>
+        <div
+          className={clsx(
+            show
+              ? "absolute h-[calc(100vh-92px)] w-1/3 top-[64px] bg-BG shadow-md rounded-r-md z-10 flex flex-col justify-between px-2 py-4 transition-transform duration-150 ease-out translate-x-0"
+              : "absolute h-[calc(100vh-92px)] w-1/3 top-[64px] bg-BG shadow-md z-10 flex flex-col justify-between px-2 py-4 transition-transform duration-150 ease-out -translate-x-full"
           )}
-          <p className="font-semibold text-center">My Items</p>
-          <div className="h-[1px] bg-BG-text rounded-md opacity-25"></div>
+        >
+          <div className="flex flex-col space-y-2 justify-between text-secondary-text">
+            {userItems.length === 0 && (
+              <div className="flex text-center items-center w-full h-[73.5vh] rounded-md">
+                <Spinner />
+              </div>
+            )}
+            <p className="font-semibold text-center">My Items</p>
+            <div className="h-[1px] bg-BG-text rounded-md opacity-25"></div>
 
-          {userItems.length > 0 &&
-            userItems.map((userItem, i) => (
-              <ProductUserListItem
-                key={i}
-                id={userItem.identifier}
-                title={userItem.title}
-                imgSrc={userItem.images[0]}
-                onClick={() => toggleSelect(userItem.identifier)}
-                selected={selID === userItem.identifier ? true : false}
-              />
-            ))}
+            {userItems.length > 0 &&
+              userItems.map((userItem, i) => (
+                <ProductUserListItem
+                  key={i}
+                  id={userItem.identifier}
+                  title={userItem.title}
+                  imgSrc={Object.values(userItem.images)[0]}
+                  onClick={() => toggleSelect(userItem.identifier)}
+                  selected={selID === userItem.identifier ? true : false}
+                />
+              ))}
+          </div>
+          <div className="pr-16">
+            <Button
+              selected={false}
+              onClick={confirmOffer}
+              value={"Offer"}
+              type={"submit"}
+            />
+          </div>
         </div>
-        <div className="pr-16">
-          <Button
-            selected={false}
-            onClick={confirmOffer}
-            value={"Offer"}
-            type={"submit"}
-          />
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
 
 export default OfferDrawer;
