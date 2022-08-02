00e8634fa6eda62bd7e74857cc12c85856b6fa7a
inpatr <i.traub@posteo.de>
2022-08-02 16:50:08 +0200

    comments on ItemDrawer comp

--- components/ItemDrawer/ItemDrawer.tsx
+++ components/ItemDrawer/ItemDrawer.tsx
@@ -1,3 +1,6 @@
+// Component for Landing Page (Map)
+// Showing all Items filtered on the Map in a List
+
 import { Dialog } from "@headlessui/react";
 import { AnimatePresence, motion } from "framer-motion";
 import {
@@ -11,10 +14,12 @@ import mapboxgl, { LngLatLike } from "mapbox-gl";
 import { Feature } from "../../utils/types";
 import ListingItem from "../ListingItem/ListingItem";
 import React from "react";
-
 import { Spinner } from "../Spinner/Spinner";
 import { filterList } from "../../utils/filterList";
 import { useItemStore } from "../../stores/itemStore";
+
+//  component holds collapsed Drawer + Sliderbutton
+//  animate entry of the Drawer (framer-motion)
 
 export default function Example({
   selectedFilter,
@@ -53,26 +58,31 @@ interface ModalProps {
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYXJvbjE4IiwiYSI6ImNsMzRibG9xYjB3ZjUzaW13d2s3bzVjcGkifQ.QGlBNyR336mJ2rFfFprAPg";
 
+//  Main Component
+// get's imported on pages
+
 export const ItemDrawer = ({ onClose, selectedFilter }: ItemDrawerProps) => {
   const [zoom, setZoom] = useState(14);
   const [listData, setListData] = useState<Feature[]>([]);
   const [initialListData, setInitialListData] = useState<Feature[]>([]);
+  //  Data from global store
   const { items } = useItemStore();
 
+  //  Sets data from global store
   useEffect(() => {
     setInitialListData(items.features);
   }, []);
 
-  console.log(selectedFilter);
-
+  //  filters data
   useEffect(() => {
     if (initialListData.length) {
       filterList(selectedFilter, initialListData, setListData);
     }
   }, [initialListData, selectedFilter]);
-  // jsx for styling the drawer
+
   return (
     <Modal onClose={onClose}>
+      {/* JSX styling for drawer */}
       <div className="flex flex-col h-[calc(100vh-160px)] pt-4">
         <div className="pb-4 shadow-sm">
           <div className="fixed top-3 left-1/4">
@@ -84,6 +94,7 @@ export const ItemDrawer = ({ onClose, selectedFilter }: ItemDrawerProps) => {
         </div>
 
         <div className="flex-1 overflow-y-scroll">
+          {/* show spinner if data fetch didn't work */}
           {!listData ? (
             <div className="flex items-center justify-center">
               <Spinner height={73} />
@@ -93,11 +104,13 @@ export const ItemDrawer = ({ onClose, selectedFilter }: ItemDrawerProps) => {
               <ul className="text-left">
                 {
                   <div id="listings" className="listings">
+                    {/* show spinner if there are no items in database (f.e. because of filter)*/}
                     {listData.length === 0 && (
                       <div className="flex text-center items-center w-full h-[73.5vh] rounded-md">
                         <Spinner />
                       </div>
                     )}
+                    {/* map over available item data */}
                     {listData.length > 0 &&
                       listData.map((listData, i) => (
                         <ListingItem
@@ -118,7 +131,7 @@ export const ItemDrawer = ({ onClose, selectedFilter }: ItemDrawerProps) => {
   );
 };
 
-// function to close the drawer
+// function to animate closing of drawer (headless UI)
 function Modal({ children }: ModalProps) {
   return (
     <Dialog
