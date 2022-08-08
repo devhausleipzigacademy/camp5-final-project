1b5ede6b8e3eb47654fcd777198bfd107409ff10
inpatr <i.traub@posteo.de>
2022-08-08 15:11:06 +0200

    added zod

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,7 +1,35 @@
 import { User, PrismaClient } from ".prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { z, ZodError } from "zod";
 
 const prisma = new PrismaClient();
+
+// identifier: string;
+//     createdAt: Date;
+//     updatedAt: Date;
+//     firstname: string;
+//     lastname: string;
+//     email: string;
+//     passwordHash: string;
+//     passwordSalt: string;
+//     profilePicture: string | null;
+//     rating: number;
+//     favorite: Prisma.JsonValue;
+
+function userModel() {
+  return z
+    .object({
+      firstname: z.string(),
+      lastname: z.string(),
+      email: z.string(),
+      passwordHash: z.string(),
+      passwordSalt: z.string(),
+      profilePicture: z.array(z.string()),
+      rating: z.number(),
+      favorite: z.array(z.string()),
+    })
+    .strict();
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,17 +37,22 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     try {
-      const userData = await req.body;
+      const userData = await checkData(req.body);
       // const locationData = await userData.
-      let user: User | null = null;
-      user = await prisma.user.create({
+      let user = await prisma.user.create({
         data: {
           ...userData,
         },
       });
       res.status(200).json(user);
     } catch (err) {
-      console.log("Error:", err);
+      if (err instanceof ZodError) {
+        console.log("z-error:", err);
+        res.status(422).send(err.message);
+      } else {
+        console.log("Error:", err);
+        res.status(500).end();
+      }
     }
   }
   if (req.method === "GET") {
@@ -46,4 +79,8 @@ export default async function handler(
       res.status(500).end();
     }
   }
+}
+async function checkData(rawData: any) {
+  const data = userModel().parse(rawData);
+  return data;
 }
